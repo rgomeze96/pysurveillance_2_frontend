0527fde581205abd11981f503442afb98815d5af
fsociety-fenix <fsociety.fenix@gmail.com>
2021-01-29 17:36:18 -0500

    new new

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">app</div>;
+  return <div className="App">app new</div>;
 }
 
 export default App;
